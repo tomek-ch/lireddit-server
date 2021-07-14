b9ff6d4c3e2ae6f946ed1d2398a38d9c11136806
Tomasz Chmielnicki <chmielnicki18@gmail.com>
2021-07-14 22:28:40 +0200

    Add  text snippet field resolver

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -9,6 +9,8 @@ import {
   Ctx,
   UseMiddleware,
   Int,
+  FieldResolver,
+  Root,
 } from "type-graphql";
 import { MyContext } from "../types";
 import { isAuth } from "../middleware/isAuth";
@@ -22,8 +24,12 @@ class PostInput {
   text: string;
 }
 
-@Resolver()
+@Resolver(Post)
 export class PostResolver {
+  @FieldResolver(() => String) textSnippet(@Root() root: Post) {
+    return root.text.slice(0, 50);
+  }
+
   @Query(() => [Post])
   async posts(
     @Arg("limit", () => Int) limit: number,
